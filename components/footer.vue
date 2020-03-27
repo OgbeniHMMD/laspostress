69821398121311db60177c5a7a3a3078b19a6d49
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-03-28 00:04:33 +0100

    Added A Basic Template ([header, Slider], Footer)

---
+++ components/footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer>
+    <div class="h1 p-5 bg-dark text-light text-center">***Footer***</div>
+  </footer>
+</template>
+
---
+++ components/header.vue
@@ -0,0 +1,6 @@
+<template>
+  <header>
+    <div class="h1 p-5 bg-primary text-light text-center">***Header***</div>
+  </header>
+</template>
+
