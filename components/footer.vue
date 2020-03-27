b81ff18527f5cf1b6ab35a84ef8edc7ddfdf1870
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-03-28 00:11:58 +0100

    Added Dummy Footer

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,63 @@
 <template>
-  <footer>
-    <div class="h1 p-5 bg-dark text-light text-center">***Footer***</div>
+  <footer class="text-light">
+    <div class="bg-secondary border-top border-dark">
+      <div class="container-fluid">
+        <div class="d-flex justify-content-between flex-wrap">
+          <div class="p-3 p-md-5">
+            <div class="h3 pb-2">Quick Links</div>
+            <div class>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+            </div>
+          </div>
+
+          <div class="p-3 p-md-5">
+            <div class="h3 pb-2">About Us</div>
+            <div class>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+            </div>
+          </div>
+
+          <div class="p-3 p-md-5">
+            <div class="h3 pb-2">Addmission</div>
+            <div class>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+            </div>
+          </div>
+
+          <div class="p-3 p-md-5">
+            <div class="h3 pb-2">Documents</div>
+            <div class>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+              <div>* Some Links goes here</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-primary border-top text-monospace text-center">
+      <div class="container-fluid p-5">
+        <div class="d-flex flex-column flex-md-row justify-content-between">
+          <div class>Copyright © 2020 Lagos State Polytechnic. All rights reserved.</div>
+          <div class="pt-3 pt-md-0">Social Links</div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
