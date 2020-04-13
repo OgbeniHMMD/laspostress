ff79d8e920ebade7ad22d4968c419930b26f7213
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-04-13 01:38:47 +0100

    Tweaked dropdown's icon for mobile, among others

--- layouts/partials/navbar.vue
+++ layouts/partials/navbar.vue
@@ -42,7 +42,7 @@
         <div class="input-group input-group-lg nav-item">
           <input
             type="search"
-            class="form-control"
+            class="form-control border-light"
             placeholder="Search"
             aria-label="Search"
             aria-describedby="button-addon1"
@@ -91,8 +91,23 @@
                 data-toggle="dropdown"
                 aria-haspopup="true"
                 aria-expanded="false"
-                class="dropdown dropdown-toggle text-light h5"
-              >{{menu.title}}</a>
+                class="dropdown dropdown-toggle text-light h5 d-flex align-items-center justify-content-between"
+              >
+                {{menu.title}}
+                <svg
+                  class="ml-2"
+                  width="1em"
+                  height="0.6em"
+                  viewBox="0 0 292.362 292.362"
+                  fill="#f8f9fa"
+                >
+                  <path
+                    d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424
+		C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428
+		s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z"
+                  />
+                </svg>
+              </a>
 
               <div
                 :class="menu.class"
@@ -118,7 +133,7 @@
 
 
 <script>
-import menuJSON from "~/assets/json/menu.json";
+import menuJSON from "~/assets/json/navbar.json";
 
 export default {
   data: function() {
