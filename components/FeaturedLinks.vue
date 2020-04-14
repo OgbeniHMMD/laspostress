b6e4f9d156b36b6cabf6957e08031787621e742f
Olajide A. Hammed <Olajide.A.Hammed@gmail.com>
2020-04-14 15:49:29 +0100

    Minor Fix

--- components/FeaturedLinks.vue
+++ components/FeaturedLinks.vue
@@ -1,31 +1,31 @@
 <template>
   <div class="container-fluid bg-white border-top px-3 py-3 py-md-5">
     <div class="card-deck">
-      <article class="card">
+      <article class="card bg-light">
         <img src="~~/assets/images/logo-inner.png" class="mx-auto" />
-        <div class="card-body lead">
+        <div class="card-body">
           <a href="#">
-            <div class="card-text">Dummy dummy dummy dummy...</div>
+            <div class="card-text">Dummy dummy dummy dummy dummy dummy dummy</div>
           </a>
           <div class="text-muted text-small">Date and Time</div>
         </div>
       </article>
 
-      <article class="card lead">
+      <article class="card bg-light">
         <img src="~~/assets/images/logo-inner.png" class="mx-auto" />
         <div class="card-body">
           <a href="#">
-            <div class="card-text">Dummy dummy dummy dummy...</div>
+            <div class="card-text">Dummy dummy dummy dummy dummy dummy dummy</div>
           </a>
           <div class="text-muted text-small">Date and Time</div>
         </div>
       </article>
 
-      <article class="card lead">
+      <article class="card bg-light">
         <img src="~~/assets/images/logo-inner.png" class="mx-auto" />
         <div class="card-body">
           <a href="#">
-            <div class="card-text">Dummy dummy dummy dummy...</div>
+            <div class="card-text">Dummy dummy dummy dummy dummy dummy dummy</div>
           </a>
           <div class="text-muted text-small">Date and Time</div>
         </div>
